030bdf3420386b4cf48f0fc33929acb999229ae2
NicolasRoss <43209525+NicolasRoss@users.noreply.github.com>
2019-09-17 11:50:24 -0400

    Update Filter.py

--- Filter.py
+++ Filter.py
@@ -14,7 +14,7 @@ size = sum(len(x) for x in kernel)
 # Initializes a temperary array and the output array.
 width, height = imgarr.shape
 tmpArr = np.array([[0 for x in range(height + 2)] for y in range(width + 2)]).astype('int8') # Adds bordering 0's to prevent black border
-outArr = np.array([[0 for x in range(height)] for y in range(width)]).astype('int8)
+outArr = np.array([[0 for x in range(height)] for y in range(width)]).astype('int8')
 
 # Adds the pixel data to the temperary array.
 for i in range(0, width):
