01267cbc8fe76734a374a53fecff4497a062d01d
NicolasRoss <g.nicolas.ross@gmail.com>
2019-10-09 00:35:01 -0400

    Done resursive

--- A2/numblackpixels.py
+++ A2/numblackpixels.py
@@ -1,132 +1,78 @@
 from PIL import Image
 import numpy as np
+import sys
 
-# Checks to see if the pixel is white
-def isBackground(pixel):
-    if (pixel == 255):
-        return True
+def isWhite(imgarr, i , j):
+    return inBounds(imgarr, i , j) and imgarr[i][j] == 255
 
-    return False
+def inBounds(imgarr, i , j):
+    return i >= 0 and j >= 0 and i < len(imgarr) and j < len(imgarr[i])
 
-# Loops through the image array to find connected regions
-def connectedComponents(imgarr):
-    comp = 1
-
-    height, width = imgarr.shape
-    for i in range(0, height):
-        for j in range(0, width):
-            if not (isBackground(imgarr[i][j])):
-                if i == 0 and j == 0:
-                    # checks if first pixel is black 
-                    imgarr[i][j] = comp
-                
-                elif i == 0 and not isBackground(imgarr[i][j - 1]):
-                    # checks if pixels in first row are black and connected
-                    imgarr[i][j] = imgarr[i][j - 1]
-
-                elif i == 0 and isBackground(imgarr[i][j - 1]): 
-                    comp += 1
-                    imgarr[i][j] = comp
-
-                elif j == 0 and not isBackground(imgarr[i - 1][j]):
-                    # checks if pixels in first col are black and connected
-                    imgarr[i][j] = imgarr[i - 1][j]
-                
-                elif j == 0 and not isBackground(imgarr[i - 1][j + 1]):
-                        imgarr[i][j] = imgarr[i - 1][j + 1]
-
-                elif j == 0 and isBackground(imgarr[i - 1][j]):
-                    comp += 1
-                    imgarr[i][j] = comp
-
-                if i != 0 and j != 0:
-                    # checks if pixels are connected and black
-                    up = imgarr[i - 1][j]
-                    left = imgarr[i][j - 1]
-                    upleft = imgarr[i - 1][j - 1]
-
-                    if j < width - 1:
-                        upright = imgarr[i - 1][j + 1]
-                        
-                        if not isBackground(upleft) and isBackground(upright):
-                            imgarr[i][j] = upleft
-                        
-                        elif isBackground(upleft) and not isBackground(upright):
-                            imgarr[i][j] = upright
-
-                        elif not isBackground(upleft) and not isBackground(upright):
-                            imgarr[i][j] = upright if upright < upleft else upleft
-                        
-                        elif isBackground(left) and isBackground(up) and isBackground(upleft) and isBackground(upright):
-                            comp += 1
-                            imgarr[i][j] = comp
-
-                    elif j == (width - 1):
-                        if not isBackground(upleft):
-                            imgarr[i][j] = upleft
-
-                        elif isBackground(left) and isBackground(up) and isBackground(upleft):
-                            comp += 1
-                            imgarr[i][j] = comp
-                        
-                        
-                    if not isBackground(left) and isBackground(up):
-                        imgarr[i][j] = left
-                    
-                    elif isBackground(left) and not isBackground(up):
-                        imgarr[i][j] = up
-
-                    elif not isBackground(left) and not isBackground(up):
-                        imgarr[i][j] = left if left < up else up
-
-                
-    return imgarr, comp
-
-def updateComponents(imgarr):
-    height, width = imgarr.shape
-    for i in range(height):
-        for j in range(width):
-            if not (isBackground(imgarr[i][j])):
-                if i != (height - 1) and j != 0:
-                    downleft = imgarr[i + 1][j - 1]
-                    down = imgarr[i + 1][j]
-
-                    if not isBackground(down):
-                        imgarr[i][j] = down if down < imgarr[i][j] else imgarr[i][j]
-
-                    if not isBackground(downleft):
-                        imgarr[i][j] = downleft if downleft < imgarr[i][j] else imgarr[i][j]
-
-    return imgarr
-
-def countComponents(imgarr, comp):
-    count = [0] * comp
-    height, width = imgarr.shape
-    for i in range(height):
-        for j in range(width):
-            pixel = imgarr[i][j]
-
-            if not isBackground(pixel):
-                count[pixel - 1] += 1
+def countRecursive(imgarr, visited, i, j, compCount):
+    visited[i][j] = 1
     
+    if not isWhite(imgarr, i, j):
+        visited[i][j] += compCount
+        # print(visited)
+        for dI in range(-1, 2):
+            for dJ in range(-1, 2):
+                adjI = i + dI
+                adjJ = j + dJ
+                
+                if inBounds(imgarr, adjI, adjJ) and not visited[adjI][adjJ]:
+                    visited = countRecursive(imgarr, visited, adjI, adjJ, compCount)
+
+    return visited
+
+def countComponents(imgarr):
+    compCount = 0
+    height, width = imgarr.shape
+    visited = [[0] * width for i in range(height)]
+    
+    for i in range(height):
+        for j in range(width):
+            if visited[i][j] < 1:
+                if not isWhite(imgarr, i, j):
+                    compCount += 1
+                    visited = countRecursive(imgarr, visited, i, j, compCount)
+
+                if visited[i][j] < 1:
+                    visited[i][j] = 1
+    
+    return visited, compCount
+
+def countBlackPixels(imgarr, compCount):
+    count = [0] * compCount
+    height = len(imgarr)
+    width = len(imgarr[0])
+    for i in range(height):
+        for j in range(width):
+            if imgarr[i][j] > 1:
+                count[imgarr[i][j] - compCount] += 1
+
     return count
 
-## main ##
-# Opens the image and sets its pixel data to an array.
-# img = Image.open('output.png').convert('L')
-# imgarr = np.array(img)
-imgarr = np.array([[  0,   0,   0,   0,   0,   0],
-                   [  0, 255, 255, 255, 255,   0],
-                   [  0, 255,   0, 255, 255,   0],
-                   [  0, 255, 255,   0, 255,   0],
-                   [  0, 255, 255, 255,   0,   0],
-                   [  0,   0,   0,   0,   0,   0]])
-imgarr, comp = connectedComponents(imgarr)
-# imgarr = updateComponents(imgarr)
-count= countComponents(imgarr, comp)
-print(imgarr)
-comp = 0
-for num in count:
-    if num > 0:
-        comp += 1
-        print("Component: {} Number of black pixels: {}".format(comp, num))
+def main():
+    # Opens the image and sets its pixel data to an array.
+    # img = Image.open('output.png').convert('L')
+    # imgarr = np.array(img)
+    imgarr = np.array([[255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+                       [255,   0, 255, 255,   0, 255, 255,   0, 255, 255],
+                       [255, 255,   0,   0, 255, 255,   0,   0, 255, 255],
+                       [255, 255, 255,   0, 255, 255, 255,   0, 255, 255],
+                       [255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+                       [255,   0, 255, 255, 255, 255,   0, 255, 255, 255],
+                       [255, 255,   0, 255, 255,   0,   0,   0, 255, 255],
+                       [255, 255, 255,   0, 255, 255,   0, 255, 255, 255],
+                       [255, 255, 255,   0, 255, 255,   0, 255,   0, 255],
+                       [255, 255, 255, 255, 255, 255, 255, 255, 255,   0]])
+
+    visited, compCount = countComponents(imgarr)
+    count = countBlackPixels(visited, compCount)
+    for i in range(len(visited)):
+        print(visited[i])
+    for i in range(len(count)):
+        print("Component {} has {} black pixels.".format(i + 1, count[i]))
+
+if __name__ == "__main__":
+    main()
