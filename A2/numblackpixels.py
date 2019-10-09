110a1697ae04f9f67cdc299a946d4ac5daf5b43a
NicolasRoss <g.nicolas.ross@gmail.com>
2019-10-09 02:48:37 -0400

    All options covered

--- A2/numblackpixels.py
+++ A2/numblackpixels.py
@@ -8,38 +8,68 @@ def isWhite(imgarr, i , j):
 def inBounds(imgarr, i , j):
     return i >= 0 and j >= 0 and i < len(imgarr) and j < len(imgarr[i])
 
-def countRecursive(imgarr, visited, i, j, compCount):
-    visited[i][j] = 1
-    
-    if not isWhite(imgarr, i, j):
-        visited[i][j] += compCount
-        # print(visited)
-        for dI in range(-1, 2):
-            for dJ in range(-1, 2):
-                adjI = i + dI
-                adjJ = j + dJ
-                
-                if inBounds(imgarr, adjI, adjJ) and not visited[adjI][adjJ]:
-                    visited = countRecursive(imgarr, visited, adjI, adjJ, compCount)
-
-    return visited
-
 def countComponents(imgarr):
-    compCount = 0
     height, width = imgarr.shape
-    visited = [[0] * width for i in range(height)]
-    
+    equivList = [0]
     for i in range(height):
         for j in range(width):
-            if visited[i][j] < 1:
-                if not isWhite(imgarr, i, j):
-                    compCount += 1
-                    visited = countRecursive(imgarr, visited, i, j, compCount)
+            if not isWhite(imgarr, i, j):
+                up = imgarr[i - 1][j]
+                left = imgarr[i][j - 1]
+                upleft = imgarr[i - 1][j - 1]
 
-                if visited[i][j] < 1:
-                    visited[i][j] = 1
-    
-    return visited, compCount
+                # surrounding pixels are inbounds
+                if inBounds(imgarr, i - 1, j) and inBounds(imgarr, i, j - 1) and inBounds(imgarr, i - 1, j - 1):
+                    # if surrounding pixels black
+                    if not isWhite(imgarr, i - 1, j) and not isWhite(imgarr, i, j - 1) and not isWhite(imgarr, i - 1, j - 1):
+                        imgarr[i][j] = min(up, min(left, upleft))
+                    # if upper-left and upper pixel black
+                    elif isWhite(imgarr, i - 1, j) and not isWhite(imgarr, i, j - 1) and not isWhite(imgarr, i - 1, j - 1):
+                        imgarr[i][j] = min(up, upleft)
+                    # if left and upper-left pixel black
+                    elif not isWhite(imgarr, i - 1, j) and isWhite(imgarr, i, j - 1) and not isWhite(imgarr, i - 1, j - 1):
+                        imgarr[i][j] = min(left, upleft)
+                    # if left and upper pixel black
+                    elif not isWhite(imgarr, i - 1, j) and not isWhite(imgarr, i, j - 1) and isWhite(imgarr, i - 1, j - 1):
+                        imgarr[i][j] = min(left, up)
+                    # if upper-left pixel black
+                    elif isWhite(imgarr, i - 1, j) and isWhite(imgarr, i, j - 1) and not isWhite(imgarr, i - 1, j - 1):
+                        imgarr[i][j] = upleft
+                    # if left pixel black
+                    elif isWhite(imgarr, i - 1, j) and not isWhite(imgarr, i, j - 1) and isWhite(imgarr, i - 1, j - 1):
+                        imgarr[i][j] = left
+                    # if upper pixel black
+                    elif not isWhite(imgarr, i - 1, j) and isWhite(imgarr, i, j - 1) and isWhite(imgarr, i - 1, j - 1):
+                        imgarr[i][j] = up
+                    # if none are black
+                    elif isWhite(imgarr, i - 1, j) and isWhite(imgarr, i, j - 1) and isWhite(imgarr, i - 1, j - 1):
+                        equivList.append(len(equivList))
+                        imgarr[i][j] = len(equivList) - 1
+                # upper pixels not in bounds
+                elif not inBounds(imgarr, i - 1, j) and inBounds(imgarr, i, j - 1) and not inBounds(imgarr, i - 1, j - 1):
+                    # if left pixel black
+                    if not isWhite(imgarr, i, j - 1):
+                        imgarr[i][j] = left
+                    # if left pixel white
+                    elif isWhite(imgarr, i, j - 1):
+                        equivList.append(len(equivList))
+                        imgarr[i][j] = len(equivList) - 1
+                # left pixels not in bounds
+                elif inBounds(imgarr, i - 1, j) and not inBounds(imgarr, i, j - 1) and not inBounds(imgarr, i - 1, j - 1):
+                    # if upper pixel black
+                    if not isWhite(imgarr, i - 1, j):
+                        imgarr[i][j] = up
+                    # if upper pixel white
+                    elif isWhite(imgarr, i - 1, j):
+                        equivList.append(len(equivList))
+                        imgarr[i][j] = len(equivList) - 1
+                # no pixels are in bounds
+                elif not inBounds(imgarr, i - 1, j) and not inBounds(imgarr, i, j - 1) and not inBounds(imgarr, i - 1, j - 1):
+                    #if pixel is black
+                    equivList.append(len(equivList))
+                    imgarr[i][j] = len(equivList) - 1
+                    
+    return imgarr
 
 def countBlackPixels(imgarr, compCount):
     count = [0] * compCount
@@ -56,7 +86,7 @@ def main():
     # Opens the image and sets its pixel data to an array.
     # img = Image.open('output.png').convert('L')
     # imgarr = np.array(img)
-    imgarr = np.array([[255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
+    imgarr = np.array([[  0, 255, 255, 255, 255, 255, 255, 255, 255, 255],
                        [255,   0, 255, 255,   0, 255, 255,   0, 255, 255],
                        [255, 255,   0,   0, 255, 255,   0,   0, 255, 255],
                        [255, 255, 255,   0, 255, 255, 255,   0, 255, 255],
@@ -66,13 +96,8 @@ def main():
                        [255, 255, 255,   0, 255, 255,   0, 255, 255, 255],
                        [255, 255, 255,   0, 255, 255,   0, 255,   0, 255],
                        [255, 255, 255, 255, 255, 255, 255, 255, 255,   0]])
-
-    visited, compCount = countComponents(imgarr)
-    count = countBlackPixels(visited, compCount)
-    for i in range(len(visited)):
-        print(visited[i])
-    for i in range(len(count)):
-        print("Component {} has {} black pixels.".format(i + 1, count[i]))
-
+    
+    imgarr = countComponents(imgarr)
+    print(imgarr)
 if __name__ == "__main__":
     main()
